4a5a63bc449536ffa59799be695519a6dbaea88c
Justin Park <192945419+justinpxrk-dev@users.noreply.github.com>
2026-05-17 07:46:19 -0700

    [claude,sketchybar] Enable Lua LSP Plugin and Fix Typo
    - Enable lua-lsp@claude-plugins-official in Claude Code settings
    - Fix "artowrk" typo in asset.lua comment

--- dot_config/sketchybar/constants/asset.lua
+++ dot_config/sketchybar/constants/asset.lua
@@ -13,7 +13,7 @@ local M = {
 			DEFAULT_IMAGE_DARK_TRANSPARENT = asset_dir .. "/logo-dark.24x24.png",
 			DEFAULT_IMAGE_LIGHT_TRANSPARENT = asset_dir .. "/logo-light.24x24.png",
 			-- TODO: DEFAULT_IMAGE_DARK_TRANSPARENT and the LIGHT equivalent are 30% transparent. Create 100% opaque
-			--		 versions for use as the default image when now_playing is playing but no artowrk data is available.
+			--		 versions for use as the default image when now_playing is playing but no artwork data is available.
 		},
 	},
 }
