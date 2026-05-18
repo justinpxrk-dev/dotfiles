39f3a96f565c9a7c7ea80ab619bc3dee6623ce60
Justin Park <192945419+justinpxrk-dev@users.noreply.github.com>
2026-05-18 01:19:35 -0700

    [brew] Bootstrap Homebrew if Not Installed

--- Scripts/brew/install_packages.sh
+++ Scripts/brew/install_packages.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
+# Installs Homebrew if not present, then installs all packages from ~/.Brewfile.
 
 set -euo pipefail
+
+if ! command -v brew &>/dev/null; then
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
 brew bundle install --global
